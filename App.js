aec8284bdb7c2ac094e9903afc079fb644543eaf
AnnabelleAB <sunhuaqing01@gmail.com>
2023-02-21 09:32:58 -0800

    connect firebase and fetch data from it

--- App.js
+++ App.js
@@ -6,6 +6,20 @@ import EditEntry from './screens/EditEntry';
 import AddEntry from './screens/AddEntry';
 import EntryTabs from './components/EntryTabs';
 import colors from './colors';
+import { initializeApp } from "firebase/app";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDgjXAfTf7N9JUpDdAmKbpS7PYo0IbYx9U",
+  authDomain: "calories-tracker-4055c.firebaseapp.com",
+  projectId: "calories-tracker-4055c",
+  storageBucket: "calories-tracker-4055c.appspot.com",
+  messagingSenderId: "220728298116",
+  appId: "1:220728298116:web:c3cff955abaa8b361810d1",
+  measurementId: "G-VFB6D1V3TZ"
+};
+const app = initializeApp(firebaseConfig);
+
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
--- components/EntriesList.js
+++ components/EntriesList.js
@@ -1,18 +1,38 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react';
 import { View, Text, FlatList, StyleSheet } from 'react-native';
 import EntryItem from './EntryItem';
 import colors from '../colors';
 import { useNavigation } from '@react-navigation/native';
+import {
+  collection,
+  getFirestore,
+  getDocs,
+} from 'firebase/firestore';
+
 const EntriesList = ({ type, navigation }) => {
-  const [entries, setEntries] = useState([
-    { id: "1", calories: "600", description: "banana" },
-    { id: "2", calories: "400", description: "banana" }
-  ]);
+  const db = getFirestore();
+  const colRef = collection(db, 'Entries');
+
+  const [entries, setEntries] = useState([]);
+
+  useEffect(() => {
+    const fetchEntries = async () => {
+      try {
+        const querySnapshot = await getDocs(colRef);
+        const data = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+        setEntries(data);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+
+    fetchEntries();
+  }, [colRef]);
 
   const overLimit = 500;
-  const filteredEntries = entries.filter(entry => entry.calories > overLimit);
+  const filteredEntries = entries.filter((entry) => entry.calories > overLimit);
   let filteredData = [];
-  if (type === "over-limit") {
+  if (type === 'over-limit') {
     filteredData = filteredEntries;
   } else {
     filteredData = entries;
@@ -28,11 +48,7 @@ const EntriesList = ({ type, navigation }) => {
         contentContainerStyle={styles.container}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
-          <EntryItem
-            entry={item}
-            navigation={navigation}
-            itemPressed={() => handlePress(item)}
-          />
+          <EntryItem entry={item} navigation={navigation} itemPressed={() => handlePress(item)} />
         )}
       />
     </View>
@@ -44,13 +60,13 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: colors.primary,
     padding: 16,
-    alignSelf: "stretch",
-    width: "100%",
+    alignSelf: 'stretch',
+    width: '100%',
   },
   flatListContainer: {
     flex: 4,
-    backgroundColor: "#dcd",
-    alignSelf: "stretch",
+    backgroundColor: '#dcd',
+    alignSelf: 'stretch',
   },
 });
 
